2338d12e069d468ac2a0bca45d5aa6a4fdb3eeb0
philhuang100 <phil.huang100@gmail.com>
2016-07-26 20:46:24 +0800

    派工即時回報系統 首發

---
+++ app/src/main/java/irs/instantreplysystem/BarCodeActivity.java
@@ -0,0 +1,13 @@
+package irs.instantreplysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code);
+    }
+}
